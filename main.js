6ab4464b61394c888f37f5c78a504f14788e4252
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-12-26 17:04:20 +0100

    -Added ejs package -Switch to DEV branch

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+    res.render('index', {});
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
