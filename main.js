c8daa7a8304ee2c42cb33f5f662247bf165361bc
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2021-12-28 00:19:10 +0100

    Added database configuration code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import express from 'express';
-import { client } from './dbconnect.js'
+import { db, rebuiltDatabase } from './dbconnect.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +12,7 @@ app.set('views', './views');
 app.use(express.static('./static'));
 app.use(express.urlencoded({extended: true}));
 
-client.connect();
+rebuiltDatabase();
 
 app.get('/', (req, res) => {
     res.render('index', {});
