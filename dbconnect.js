43647b3cdb2281afcbba96ffecca48b1f20c9acc
bullyhunter1917 <45041977+bullyhunter1917@users.noreply.github.com>
2022-01-07 17:48:27 +0100

    Repaired main site and category site

--- dbconnect.js
+++ dbconnect.js
@@ -3,6 +3,7 @@ const env = process.env.DB_ENVIRONMENT || "development";
 
 const db_data = {
     development: 'postgres://postgres:password@localhost:5432/weppo_store',
+    // development: 'postgres://postgres:142327@localhost:5432/weppo_store',
     production: process.env.DATABASE_URL
 };
 
